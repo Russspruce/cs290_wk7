48d3c544bc895b9023c0845e906bebff57bc1bbe
Andrew Russo <andrewrusso@Andrews-MBP.PK5001Z>
2017-02-26 17:37:06 -0800

    Another fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 5000);
+app.set(process.env.PORT || 5000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
